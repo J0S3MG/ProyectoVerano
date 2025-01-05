c2278eb4e40931ec4d51854eb5d67152d9880e85
J0S3MG <m0n0.gj81@gmail.com>
2025-01-05 17:25:47 -0300

    Version 0.0

---
+++ WebAplicationEjemplo/WebApplication1/Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class Alumno: IComparable
+    {
+        public string Nom {  get; set; }
+        public long LU { get; set; }
+        public int Id { get; set; }
+        public double Nota { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Alumno a = obj as Alumno;
+            if (a != null) return a.Id.CompareTo(Id);
+            else return 1;
+        }
+    }
+}
